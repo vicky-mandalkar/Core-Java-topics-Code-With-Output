15f4cc559078908012d7060a28877e6843d2c5e8
vicky Mandalkar <vickymandalkar143@gmail.com>
2026-06-07 13:39:42 +0530

    Update ForwardandBackwardDisplay.java

--- Array/ForwardandBackwardDisplay.java
+++ Array/ForwardandBackwardDisplay.java
@@ -1,3 +1,14 @@
+/*
+Array Case 1: Forward and Backward Display
+ Create an array of integers, store elements in it, and display the elements:
+- In forward order (from first to last)
+- In backward order (from last to first)
+ Example Input:
+Array = [10, 20, 30, 40, 50]
+ Expected Output:
+Forward: 10 20 30 40 50
+Backward: 50 40 30 20 10
+*/
 import java.util.Scanner;
 class ForwardandBackwardDisplay{
 
