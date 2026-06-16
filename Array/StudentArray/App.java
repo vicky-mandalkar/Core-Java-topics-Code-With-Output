34f5661ad7e45cf4ff70933f637dcfeafcbd3bd8
vicky Mandalkar <vickymandalkar143@gmail.com>
2026-06-16 14:58:29 +0530

    Update App.java

--- Array/StudentArray/App.java
+++ Array/StudentArray/App.java
@@ -4,6 +4,18 @@ public class App{
 		int Marks[]={55,66,77,88,99};
 
 		Student s1=new Student(11,"vicky",Student,Marks);
+		s1.PrintStudentInfo();
 	}
-
 }
+/*
+--------------Student Details--------------
+   ID      NAME   SUBJECT     MARKS
+------------------------------------------
+   11     vicky
+                 PHY :    55
+                CHEM :    66
+                MATH :    77
+                 BIO :    88
+                 ENG :    99
+=========================================
+*/
